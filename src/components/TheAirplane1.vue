253d994ec0cd0600652962cace5740e08c222694
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-15 14:41:22 +0200

    Renamed & Switched file versions

--- src/components/TheAirplane1.vue
+++ src/components/TheAirplane1.vue
@@ -2,6 +2,7 @@
 import { useGLTF } from '@tresjs/cientos'
 import { useLoop } from '@tresjs/core'
 import type { Object3D } from 'three'
+import { shallowRef, watch } from 'vue'
 
 const props = defineProps<{
   planet: Object3D
@@ -25,29 +26,21 @@ props.planet.geometry.computeBoundingSphere()
 const radius = Math.abs(props.planet.geometry.boundingSphere?.radius | 1) + 0.5
 airplane.position.set(radius, 0, 0)
 
-let angle = Math.PI / 2.3 // Angle for orbiting the planet
-const speed = 0.2 // Speed of orbit
-let rollAngle = 0 // Angle for aileron roll
-const rollSpeed = 2 // Speed of the roll
+let angle = Math.PI / 4
+const spin = Math.PI / 4
+const speed = 0.2
 
 onBeforeRender(({ delta }) => {
   if (!airplane || !props.planet) { return }
 
-  // Update orbit
   angle += delta * speed
   const x = radius * Math.cos(angle)
   const z = radius * Math.sin(angle)
   airplane.position.x = x
   airplane.position.z = z
-
-  // Update roll
-  rollAngle += delta * rollSpeed
-
-  // Apply rotations
-  airplane.rotation.x = rollAngle // Longitudinal roll
-  airplane.rotation.z = -Math.PI / 2.314 // Tilting for aesthetic
-  airplane.rotation.y = -angle // Keep oriented toward orbit
-
+  airplane.rotation.x = spin
+  airplane.rotation.z = -Math.PI / 2.314
+  airplane.rotation.y = -angle
   airplane.updateMatrixWorld()
 })
 </script>
