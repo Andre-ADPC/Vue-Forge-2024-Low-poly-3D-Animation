afbe33d152a1c7bc319eec6e0372b8fb7152f326
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-16 23:38:08 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,3 +7,12 @@ export default tresLintConfig({
     'style/max-statements-per-line': 'off',
   },
 })
+
+module.exports = {
+  parser: 'vue-eslint-parser',
+  plugins: ['vue'],
+  extends: ['plugin:vue/vue3-recommended', 'prettier'],
+  rules: {
+    // Add any additional rules here
+  },
+}
