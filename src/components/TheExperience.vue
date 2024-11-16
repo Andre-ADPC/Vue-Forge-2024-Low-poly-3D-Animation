25ee04ff617f6e8a91b5bc266e2f48bec4f98d4a
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-16 23:39:21 +0200

    Update TheExperience.vue

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { OrbitControls, Stars } from '@tresjs/cientos'
 import ThePlanet from './ThePlanet.vue'
-//      ^? Note: TwoSlash Queries does NOT work inside TS-based Scripts (yet) [See Orta](https://github.com/orta/vscode-twoslash-queries/issues/36#issuecomment-1823439282)
+//     ^? Note: TwoSlash Queries does NOT work inside TS-based Scripts (yet) [See Orta](https://github.com/orta/vscode-twoslash-queries/issues/36#issuecomment-1823439282)
 </script>
 
 <template>
