0cd5e09fa7abea97b25d3de054ed5a6964dd109b
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-19 00:20:58 +0200

    Adding New Component Model files

---
+++ src/OriginalApp.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import { PCFSoftShadowMap } from 'three'
+
+// Render various Experiences individually or combined by building other Experience versions.
+
+// import TheExperience from './components/TheExperienceOrigins.vue'
+// import TheExperience from './components/TheExperienceAirplane.vue'
+// import TheExperience from './components/ThePlanet.vue'
+import TheExperience from './components/TheExperience.vue'
+</script>
+
+<template>
+  <TresCanvas clear-color="#020202" window-size shadows :shadow-map-type="PCFSoftShadowMap">
+    <TheExperience />
+  </TresCanvas>
+</template>
