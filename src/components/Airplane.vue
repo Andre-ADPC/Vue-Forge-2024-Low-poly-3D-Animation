bb576a9d49c64eadc57ca400a4ac6f10d0939180
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-19 23:59:10 +0200

    Update Airplane.vue
    Second iteration

--- src/components/Airplane.vue
+++ src/components/Airplane.vue
@@ -1,48 +1,58 @@
-<script setup lang="ts"> 
-import { useGLTF } from '@tresjs/cientos' 
-import { useLoop } from '@tresjs/core' 
-import type { Object3D } from 'three' 
-import { shallowRef, watch } from 'vue' 
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos'
+import { useLoop } from '@tresjs/core'
+import type { Object3D } from 'three'
+import { shallowRef, watch } from 'vue'
 
-const props = defineProps<{ 
-    planet: Object3D 
-}>() 
+const props = defineProps<{
+  planet: Object3D
+}>()
 
-const { scene } = await useGLTF( './low-poly-planet/airplane.gltf', ) 
+const { scene } = await useGLTF(
+  '/public/assets/airplane.gltf',
+)
 
-const airplane = scene 
-scene.traverse((child) => { 
-    if (child.isMesh) { 
-        child.castShadow = true 
-    } 
-}) 
-airplane.updateMatrixWorld() 
+const airplane = scene
+scene.traverse((child) => {
+  if (child.isMesh) {
+    child.castShadow = true
+  }
+})
+airplane.updateMatrixWorld()
 
-const { onBeforeRender } = useLoop() 
+const { onBeforeRender } = useLoop()
 
-props.planet.geometry.computeBoundingSphere() 
-const radius = Math.abs(props.planet.geometry.boundingSphere?.radius | 1) + 0.5; 
-airplane.position.set(radius, 0, 0) 
+props.planet.geometry.computeBoundingSphere()
+const radius = Math.abs(props.planet.geometry.boundingSphere?.radius | 1) + 0.5
+airplane.position.set(radius, 0, 0)
 
-let angle = Math.PI / 4
-const spin = Math.PI / 4
-const speed = 0.2 
+let angle = Math.PI / 4 // Angle for orbiting the planet
+const speed = 0.2 // Speed of orbit
+let rollAngle = 0 // Angle for aileron roll
+const rollSpeed = 2 // Speed of the roll
 
-onBeforeRender(({ delta }) => { 
-    if (!airplane || !props.planet) { return } 
-    
-    angle += delta * speed 
-    const x = radius * Math.cos(angle) 
-    const z = radius * Math.sin(angle) 
-    airplane.position.x = x 
-    airplane.position.z = z 
-    airplane.rotation.x = spin 
-    airplane.rotation.z = -Math.PI / 2.314 
-    airplane.rotation.y = -angle 
-    airplane.updateMatrixWorld() 
-}) 
+onBeforeRender(({ delta }) => {
+  if (!airplane || !props.planet) { return }
+
+  // Update orbit
+  angle += delta * speed
+  const x = radius * Math.cos(angle)
+  const z = radius * Math.sin(angle)
+  airplane.position.x = x
+  airplane.position.z = z
+
+  // Update roll
+  rollAngle += delta * rollSpeed
+
+  // Apply rotations
+  airplane.rotation.x = rollAngle // Longitudinal roll
+  airplane.rotation.z = -Math.PI / 2.314 // Tilting for aesthetic
+  airplane.rotation.y = -angle // Keep oriented toward orbit
+
+  airplane.updateMatrixWorld()
+})
 </script>
 
-<template> 
-<primitive :object="airplane" /> 
+<template>
+  <primitive :object="airplane" />
 </template>
