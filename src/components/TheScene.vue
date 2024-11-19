5595b4c4ab7fd1c6fb1f655250d88f47d44662e3
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-19 21:51:19 +0200

    Update TheScene.vue
    First iteration

---
+++ src/components/TheScene.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { OrbitControls, Stars } from '@tresjs/cientos'
+import Planet from './Planet.vue'
+//     ^? Note: TwoSlash Queries does NOT work inside TS-based Scripts (yet) [See Orta](https://github.com/orta/vscode-twoslash-queries/issues/36#issuecomment-1823439282)
+</script>
+
+<template>
+  <TresPerspectiveCamera :position="[0, 0, 5]" :near="0.001" :far="2000" />
+  <OrbitControls />
+  <TresAmbientLight />
+  <TresDirectionalLight
+    :shadow-bias="0.0001"
+    cast-shadow
+    :position="[2, 2, 5]"
+    :look-at="[0, 0, 0]"
+  />
+  <Suspense>
+    <Planet />
+  </Suspense>
+  <Stars />
+  <TresAxesHelper />
+</template>
