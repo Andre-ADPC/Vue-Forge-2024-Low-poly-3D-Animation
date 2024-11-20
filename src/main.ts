ab4bcb96d871243799c32858501154933f80110d
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-21 01:46:42 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import '@tresjs/leches/styles'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.mount('#app')
