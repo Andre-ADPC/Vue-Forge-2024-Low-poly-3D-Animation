c6a1f1c55029bb43165cb06e3756d734578f1cd9
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-21 14:52:08 +0200

    Update useSettingsStore.ts

--- src/pinia_stores/useSettingsStore.ts
+++ src/pinia_stores/useSettingsStore.ts
@@ -4,42 +4,42 @@
 import { defineStore } from 'pinia'
 
 export const useSettingsStore = defineStore('settings', {
-state: () => ({
-    isModalOpen: false,
-    showAll: true,
-    visibility: {
+    state: () => ({
+      isModalOpen: false, // Modal state
+      showAll: true, // Master visibility toggle
+      visibility: {
         coordinates: true,
         airplane: true,
         clouds: true,
         planet: true,
         airplaneAxes: false,
         planetAxes: false,
-    },
-    rotationSpeed: 0.1,
-    airplaneDistance: 5.0,
-}),
-actions: {
-    toggleModal() {
+      },
+      rotationSpeed: 0.1, // Adjustable parameters
+      airplaneDistance: 5.0,
+    }),
+    actions: {
+      toggleModal() {
         this.isModalOpen = !this.isModalOpen
-    },
-    resetSettings() {
+      },
+      setMasterVisibility(newValue: boolean) {
+        this.showAll = newValue
+        Object.keys(this.visibility).forEach((key) => {
+          this.visibility[key as keyof typeof this.visibility] = newValue
+        })
+      },
+      resetSettings() {
         this.showAll = true
         this.visibility = {
-            coordinates: true,
-            airplane: true,
-            clouds: true,
-            planet: true,
-            airplaneAxes: false,
-            planetAxes: false,
-    }
+          coordinates: true,
+          airplane: true,
+          clouds: true,
+          planet: true,
+          airplaneAxes: false,
+          planetAxes: false,
+        }
         this.rotationSpeed = 0.1
         this.airplaneDistance = 5.0
+      },
     },
-        setMasterVisibility(newValue: boolean) {
-            this.showAll = newValue
-            Object.keys(this.visibility).forEach((key) => {
-            this.visibility[key as keyof typeof this.visibility] = newValue
-            })
-        },
-    },
-})
+  })
