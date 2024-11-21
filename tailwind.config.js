3564a63b653e92117bf74e3b5d99ad492d181357
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-11-21 12:35:54 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui"
+const plugin = require('tailwindcss/plugin')
 
 export default {
+  prefix: 'tw-',
+  important: false,
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx,glsl}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
     daisyui,
+    require('@tailwindcss/forms'),
   ],
+  daisyui: {
+    themes: ["light", "dark"], // Example: Pre-configured themes
+  },
 }
